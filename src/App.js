b69fddcd71d80533301ef06d0d93bc485f74743c
Yehor Voronov <380954214108tt@gmail.com>
2023-03-08 17:47:35 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './hooks/useTelegram';
- 
-const token="1450164197:AAFqvV6DHugvks5889prhHZvzAid-kBa_8I";
+import Header from './components/Header/Header';
+
+const token = "1450164197:AAFqvV6DHugvks5889prhHZvzAid-kBa_8I";
 
 function App() {
 
-  const {onToggleButton,tg}=useTelegram()
+  const { onToggleButton, tg } = useTelegram()
 
-  useEffect(()=>{
-  tg.ready()
-  },[])
+  useEffect(() => {
+    tg.ready()
+  }, [])
 
-  
+
 
   return (
     <div className="App">
-     <button onClick={onToggleButton}>toggle</button>
+      <Header />
+      <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
