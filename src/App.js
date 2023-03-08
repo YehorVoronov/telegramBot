7938c76909f2dee62316a1e274d7430c968a6be2
Yehor Voronov <380954214108tt@gmail.com>
2023-03-08 14:16:49 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+const tg=window.Telegram.WebApp;
+
+const token="1450164197:AAFqvV6DHugvks5889prhHZvzAid-kBa_8I";
+
 
 function App() {
+
+  useEffect(()=>{
+  tg.ready()
+  },[])
+
+  const onClose=()=>{
+    tg.close()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     chat
+     <button onClick={onClose}>close</button>
     </div>
   );
 }
